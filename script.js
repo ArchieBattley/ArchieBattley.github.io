305fbf08d6461f85dc3e65e76214e0a80ac9efad
ArchieBattley <archiebattley14@gmail.com>
2024-02-12 15:32:38 +0000

    added interactive elemtents

--- script.js
+++ script.js
@@ -14,10 +14,22 @@ const onProgress = (event) => {
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
 
+// Enables Color Picker Functionality
 const modelViewerColor = document.querySelector("model-viewer#color");
-      
-      document.querySelector('#color-controls').addEventListener('click', (event) => {
-        const colorString = event.target.dataset.color;
-        const [material] = modelViewerColor.model.materials;
-        material.pbrMetallicRoughness.setBaseColorFactor(colorString);
-      });
+
+// Body Color Selector
+document.querySelector('#color-controls').addEventListener('click', (event) => {
+  const colorString = event.target.dataset.color;
+  const material = modelViewerColor.model.materials[2]; // added the index of the material that I see in the editor page
+  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+});
+
+// Wheel Color Selector
+document.querySelector('#color-controls2').addEventListener('click', (event) => {
+  const colorString = event.target.dataset.color;
+  const material = modelViewerColor.model.materials[9]; // added the index of the material that I see in the editor page
+  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+});
+    
+
+    
