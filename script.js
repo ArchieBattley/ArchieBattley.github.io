6339167c85114458aeabafc3a8c8e3a02dee6b55
ArchieBattley <archiebattley14@gmail.com>
2024-02-26 23:46:33 +0000

    Added New model version

--- script.js
+++ script.js
@@ -14,20 +14,21 @@ const onProgress = (event) => {
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
 
+
 // Enables Color Picker Functionality
 const modelViewerColor = document.querySelector("model-viewer#color");
 
 // Body Color Selector
 document.querySelector('#color-controls').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[8]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[6]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
 
 // Wheel Color Selector
 document.querySelector('#color-controls2').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[26]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[16]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
     
