e3c7cb8b6797f9c87b03a8c768829aaae0c4083b
ArchieBattley <45824885+ArchieBattley@users.noreply.github.com>
2024-02-27 13:52:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -21,14 +21,14 @@ const modelViewerColor = document.querySelector("model-viewer#color");
 // Body Color Selector
 document.querySelector('#color-controls').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[6]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[1]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
 
 // Wheel Color Selector
 document.querySelector('#color-controls2').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[16]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[21]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
     
