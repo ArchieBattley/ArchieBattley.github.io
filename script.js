719c74816aeb327a960870b33207196bb6c09a02
ArchieBattley <archiebattley14@gmail.com>
2024-02-28 23:25:22 +0000

    added latest model version and bumper color picker

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ const modelViewerColor = document.querySelector("model-viewer#color");
 // Body Color Selector
 document.querySelector('#color-controls').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[8]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[10]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
 
 // Wheel Color Selector
 document.querySelector('#color-controls2').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[5]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[8]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
     
+// Bumper Color Selector
+document.querySelector('#color-controls3').addEventListener('click', (event) => {
+  const colorString = event.target.dataset.color;
+  const material = modelViewerColor.model.materials[2]; // added the index of the material that I see in the editor page
+  material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+});
 
     
