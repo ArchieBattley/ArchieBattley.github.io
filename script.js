c741af3033d3b71ce760320e4ea0464df45ad36c
ArchieBattley <archiebattley14@gmail.com>
2024-02-28 23:33:09 +0000

    fixed brake bug

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const modelViewerColor = document.querySelector("model-viewer#color");
 // Body Color Selector
 document.querySelector('#color-controls').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[10]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[11]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
 
 // Wheel Color Selector
 document.querySelector('#color-controls2').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[8]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[9]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
     
