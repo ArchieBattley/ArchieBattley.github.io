716de168fdc2c752aff40becd3e3015ecbe89b12
ArchieBattley <45824885+ArchieBattley@users.noreply.github.com>
2024-03-02 11:21:01 +0000

    Update configuratorscript.js

--- kingsley/js/configuratorscript.js
+++ kingsley/js/configuratorscript.js
@@ -1,3 +1,21 @@
+// Disables Zoom on Mobile
+document.addEventListener('gesturestart', function(e) {
+    e.preventDefault();
+    // special hack to prevent zoom-to-tabs gesture in safari
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener('gesturechange', function(e) {
+    e.preventDefault();
+    // special hack to prevent zoom-to-tabs gesture in safari
+    document.body.style.zoom = 0.99;
+});
+
+document.addEventListener('gestureend', function(e) {
+    e.preventDefault();
+    // special hack to prevent zoom-to-tabs gesture in safari
+    document.body.style.zoom = 0.99;
+});
 
 // Enables Color Picker Functionality
 const modelViewerColor = document.querySelector("model-viewer#color");
