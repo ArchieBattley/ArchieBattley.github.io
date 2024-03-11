19ee97a17cef2f0f10dcb95091b13e5f8a5aa4a6
ArchieBattley <archiebattley14@gmail.com>
2024-03-11 23:11:29 +0000

    aded latest model version

--- kingsley/js/configuratorscript.js
+++ kingsley/js/configuratorscript.js
@@ -41,21 +41,21 @@ const modelViewerColor = document.querySelector("model-viewer#color");
 // Body Color Selector
 document.querySelector('#color-controls').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[14]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[10]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
 
 // Wheel Color Selector
 document.querySelector('#color-controls2').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[9]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[8]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
     
 // Bumper Color Selector
 document.querySelector('#color-controls3').addEventListener('click', (event) => {
   const colorString = event.target.dataset.color;
-  const material = modelViewerColor.model.materials[3]; // added the index of the material that I see in the editor page
+  const material = modelViewerColor.model.materials[2]; // added the index of the material that I see in the editor page
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
 
