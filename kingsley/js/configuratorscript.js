53564b7df5088f81b9d7dba5a7e15c7a573c671e
ArchieBattley <archiebattley14@gmail.com>
2024-03-15 02:12:05 +0000

    Added styling changes and random color
    added a function that applies a random color from an array to the body color on page load

--- kingsley/js/configuratorscript.js
+++ kingsley/js/configuratorscript.js
@@ -35,8 +35,29 @@ document.addEventListener('gestureend', function(e) {
     document.body.style.zoom = 0.99;
 });
 
+// CONFIGURATOR 
+
 // Enables Color Picker Functionality
 const modelViewerColor = document.querySelector("model-viewer#color");
+
+// Function to generate a random color
+function getRandomColor() {
+    const colors = ['#005b6d', '#8A210C', '#4B6413', '#3E4428', '#dddbbf', '#D17E27', '#442719', '#a1987e']; // Add more colors as needed
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  // Function to apply a random color to the model
+  function randomizeColor() {
+    const modelViewerColor = document.getElementById('color');
+    const randomColor = getRandomColor();
+    const material = modelViewerColor.model.materials[0];
+    material.pbrMetallicRoughness.setBaseColorFactor(randomColor);
+  }
+
+  // Wait for a short delay after the page has loaded, then apply a random color
+  window.addEventListener('load', () => {
+    setTimeout(randomizeColor, 500); // Adjust the delay as needed
+  });
 
 // Body Color Selector
 document.querySelector('#color-controls').addEventListener('click', (event) => {
